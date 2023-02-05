96fa920c019ea4a46709eacd4d46035c553ae545
hwanghyekyung <hwanghyekyung1004@gmail.com>
2023-02-06 00:25:53 +0900

    css보정

--- js/todo.js
+++ js/todo.js
@@ -5,12 +5,27 @@ const $todoList = document.querySelector('.todoList');
 
 function handleTodoSubmit(event){
     event.preventDefault();
-    console.log("handleTodoSubmit---call "); 
     const newTodo = $todoInput.value;
     $todoInput.value = '';
-    // const $newTodo = document.createElement('li');
-    // $newTodo.innerText = newTodo;
-    // $todoList.appendChild($newTodo);
+    addToDo(newTodo);
 }
+
+function deleteTodo(event){
+    event.target.parentElement.parentElement.remove();
+}
+
+function addToDo(newTodo){
+    const li = document.createElement('li'); 
+    const span = document.createElement('span'); 
+    const trash = document.createElement('button');
+
+    trash.innerHTML = '<img src="icon/trash.png" style="border:none">';
+    trash.addEventListener('click', deleteTodo);
+    li.appendChild(span);  
+    li.appendChild(trash);  
+    span.innerText = newTodo;
+    $todoList.appendChild( li);
+    
+} 
 
 $todoForm.addEventListener('submit', handleTodoSubmit); 
