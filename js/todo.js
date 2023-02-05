5a0842a316a931616d40be074a3d2c4ec9a88ca4
hwanghyekyung <hwanghyekyung1004@gmail.com>
2023-02-06 00:45:07 +0900

    todo 저장, 로드

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,9 @@ const $todoForm = document.querySelector('#todoForm');
 const $todoInput = document.querySelector('#todoForm input');
 const $todoList = document.querySelector('.todoList');
 
+const todo = []; 
+
+loadTodo();
 
 function handleTodoSubmit(event){
     event.preventDefault();
@@ -12,6 +15,12 @@ function handleTodoSubmit(event){
 
 function deleteTodo(event){
     event.target.parentElement.parentElement.remove();
+    const li = event.target.parentElement.parentElement;
+    const span = li.querySelector('span');
+    const newTodo = span.innerText;
+    // console.log("newtodo:", newTodo);
+    todo.splice(todo.indexOf(newTodo), 1);
+    saveTodo(); 
 }
 
 function addToDo(newTodo){
@@ -25,7 +34,20 @@ function addToDo(newTodo){
     li.appendChild(trash);  
     span.innerText = newTodo;
     $todoList.appendChild( li);
-    
+
+    todo.push(newTodo);  
+    saveTodo();
 } 
+function saveTodo(){
+    localStorage.setItem('todo', JSON.stringify(todo));
+}
+
+function loadTodo(){
+    const savedTodo = localStorage.getItem('todo');
+    if(savedTodo !== null){
+        const parsedTodo = JSON.parse(savedTodo);
+        parsedTodo.forEach((item) => addToDo(item));
+    }
+}
 
 $todoForm.addEventListener('submit', handleTodoSubmit); 
