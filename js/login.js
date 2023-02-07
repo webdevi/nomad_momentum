7cf64244c057827a15d3f69480043bc566bdbf33
gracehwang <thewarmers@naver.com>
2023-02-08 08:29:39 +0900

    수정

--- js/login.js
+++ js/login.js
@@ -1,35 +1,33 @@
 const $loginForm = document.querySelector("#loginForm");
 const $loginInput = document.querySelector("#loginForm input");
 const $welcomeMsg = document.querySelector(".welcomeMsg");
-const HIDDEN_CLASSNAME = "hidden"; 
- 
+const HIDDEN_CLASSNAME = "hidden";
+
 $loginInput.focus();
 
-function onLoginSubmit(event){
-    event.preventDefault();
-    const name = $loginInput.value; 
-    setName(name); 
-}  
-
-function dispName(name){
-    $welcomeMsg.innerHTML = `Hello ${name}`;
-    $loginForm.classList.add(HIDDEN_CLASSNAME);
-}
-function setName(name){
-    localStorage.setItem("username", name);
-    dispName(name);
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const name = $loginInput.value;
+  setName(name);
 }
 
-window.onload = function(){ 
-    const myName = localStorage.getItem("username");
-    if(myName !== null || myName == ""){ 
-        $loginForm.classList.add(HIDDEN_CLASSNAME);
-        dispName(myName);
-    } 
-    else {
-        $loginForm.classList.remove(HIDDEN_CLASSNAME);
-        $loginInput.focus();
-    }
-} 
+function dispName(name) {
+  $welcomeMsg.innerHTML = `Hello ${name}`;
+  $loginForm.classList.add(HIDDEN_CLASSNAME);
+}
+function setName(name) {
+  localStorage.setItem("username", name);
+  dispName(name);
+}
 
-$loginForm.addEventListener("submit", onLoginSubmit); 
+const myName = localStorage.getItem("username");
+console.log("myname : " + myName);
+if (myName !== null || myName == "") {
+  $loginForm.classList.add(HIDDEN_CLASSNAME);
+  dispName(myName);
+} else {
+  $loginForm.classList.remove(HIDDEN_CLASSNAME);
+  $loginInput.focus();
+}
+
+$loginForm.addEventListener("submit", onLoginSubmit);
--- js/weather.js
+++ js/weather.js
@@ -1,23 +1,24 @@
-const WEATHER_API_KEY = config.apikey;
-const $weatherIcon = document.querySelector('#weatherIcon');
-const $weatherTemp = document.querySelector('#weatherTemp');        
-const $weather = document.querySelector('.weather');
+const WEATHER_API_KEY = "e9e1e7536a783045ef01ea994068798b";
+const $weatherIcon = document.querySelector("#weatherIcon");
+const $weatherTemp = document.querySelector("#weatherTemp");
+const $weather = document.querySelector(".weather");
 
 function onGeoOk(position) {
-    
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    //console.log("You live in : ", lat, lon);
-     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`;
-      fetch(url).then(response => response.json()).then(data => {
-        $weather.style.display = "flex";
-        $weatherIcon.innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="weather icon">`;        
-        $weatherTemp.innerText = `${data.main.temp.toFixed(1)}°C`; 
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  //console.log("You live in : ", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      $weather.style.display = "flex";
+      $weatherIcon.innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="weather icon">`;
+      $weatherTemp.innerText = `${data.main.temp.toFixed(1)}°C`;
     });
 }
 function onGeoError() {
-    alert("Can't find you. No weather for you.");
-    $weather.style.display = "none"; 
+  alert("Can't find you. No weather for you.");
+  $weather.style.display = "none";
 }
 
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);  
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
